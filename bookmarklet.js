850b6430ae16708bd26f1982a2867e81b4400a44
Tyler Biscoe <biscoe@gmail.com>
2013-05-26 17:15:08 -0400

    Removed easyXDM

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,13 +1,14 @@
 (function() {
+	'use strict';
 	// Helper functions
 	function is_set(o) {
-		return typeof o !== 'undefined' ? true : false;
+		return o !== undefined ? true : false;
 	}
 	function not_set(o) {
-		return typeof o === 'undefined' ? true : false;
+		return o === undefined ? true : false;
 	}
 	function observe(obj, type, fn) {
-	  if (obj.attachEvent) {
+	  if(obj.attachEvent) {
 		obj['e'+type+fn] = fn;
 		obj[type+fn] = function(){obj['e'+type+fn](window.event);}
 		obj.attachEvent('on'+type, obj[type+fn]);
@@ -24,9 +25,29 @@
 	function getEventElement(e) {
 		return e.target || e.srcElement;
 	}
+	function applyStyle(ele, style, val) {
+		switch(style) {
+			case 'boxShadow':
+				ele.style['boxShadow'] = val;
+				ele.style['WebkitBoxShadow'] = val;
+				ele.style['MozBoxShadow'] = val;
+			break;
+			case 'opacitiy':
+				ele.style['opacity'] = val;
+				ele.style['filter'] = 'alpha(opacity='+Math.round(val*100)+')';
+				
+			default:
+				ele.style[style] = val;
+			break;
+		}
+	}
 	function addElement(element_type, parent_element, style, tags) {
 		var r = document.createElement(element_type);
-		if(style) for(var x in style) r.style[x] = style[x];
+		if(style) {
+			for(var x in style) {
+				applyStyle(r, x, style[x]);
+			}
+		}
 		if(tags) for(var x in tags) {r[x] = tags[x];}
 		parent_element.appendChild(r);
 		return(r);
@@ -93,7 +114,27 @@
 	function getIframeDocument(iFrame) {
 		return iFrame.contentDocument ? iFrame.contentDocument : iFrame.contentWindow.document;
 	}
-	//--
+	function loadScript(src, callback_fn) {
+		var s1 = document.createElement('script');
+		s1.type = 'text/javascript';
+		s1.src = src;
+		document.getElementsByTagName('head')[0].appendChild(s1);
+		s1.onreadystatechange = function(){
+			if (this.readyState === 'complete' || this.readyState === 'loaded') {
+				callback_fn();
+			}
+		};
+		s1.onload = callback_fn;
+	}
+	
+	// Properties
+	var properties = {
+		upload_script: '',
+		upload_paths: [],
+		min_image_h: 60,
+		min_image_w: 60,
+		check_iframes: true
+	};
 	// Hide flash embeds, fix for this may come later.
 	function hideFlash() {
 		var embed = document.getElementsByTagName('embed');
@@ -108,28 +149,166 @@
 			object[i].style.visibility = 'hidden';
 		}
 	}
-	function getImages() {
-		var imgs = [];
-		debugger;
+
+
+	// Returns an array of all images on the page
+	function getAndDrawImages() {
+		var imgs = [], ind;
+		var draw_image = function(img_obj) {
+			var image = {};
+			image.container = addElement('div', images_screen, {
+				padding: '10px',
+				height: '200px',
+				width: '200px',
+				'float': 'left',
+				backgroundColor: '',
+				borderRight: 'thin solid rgb(230,230,230)',
+				borderBottom: 'thin solid rgb(230,230,230)',
+				textAlign: 'center',
+				cursor: 'pointer'
+			});
+			image.image = addElement('img', image.container, {
+				maxWidth: '200px',
+				maxHeight:'200px'
+			}, {src: img_obj.src});
+			image.container.onmouseover = function(e) {
+				image.container.style.backgroundColor = 'rgb(240,240,255)';
+			};
+			image.container.onmouseout = function(e) {
+				image.container.style.backgroundColor = '';
+			};
+			image.container.onclick = function(e) {
+				uploadSetupPage(img_obj);
+			};
+		};
 		for(var i=0; i<document.images.length; i++){
 			var dim = elementDimensions(document.images[i]);
-			if((dim.width>60 || dim.height>60) && dim.width>40 && dim.height>40) {
-				imgs.push({src: document.images[i].src, page_url: window.location.href, dim: dim});
-			}
+			imgs.push({src: document.images[i].src, page_url: window.location.href, dim: dim});
+			
 		}
-		var iframes = document.getElementsByTagName("iframe");
-		for(var j = 0;j < iframes.length;j++) {
-			try {
-				var doc = iDoc(iframes[j]);
-				var dim = elementDimensions(document.images[i]);
-				if((dim.width>60 || dim.height>60) && dim.width>40 && dim.height>40) {
+		if(properties.check_iframes === true) {
+			var iframes = document.getElementsByTagName("iframe");
+			for(var j = 0;j < iframes.length;j++) {
+				try {
+					var doc = iDoc(iframes[j]);
+					var dim = elementDimensions(document.images[i]);
 					imgs.push({src: document.images[i].src, page_url: window.location.href, dim: dim});
+				} catch(err) {
+					console.log('Error', err);
 				}
-			} catch(err) {
-				console.log('Error', err);
 			}
 		}
+		for(var n = 0;n<imgs.length;n++) {
+			draw_image(imgs[n]);
+		}
+		for(var n = 0;n<imgs.length;n++) {
+			draw_image(imgs[n]);
+		}
+		for(var n = 0;n<imgs.length;n++) {
+			draw_image(imgs[n]);
+		}
+		for(var n = 0;n<imgs.length;n++) {
+			draw_image(imgs[n]);
+		}
+		
 		return imgs;
 	}
-	console.log(getImages());
+	
+	// Upload Screen
+	
+	function uploadSetupPage(img_obj) {
+		var socket = new easyXDM.Socket({
+			remote: 'http://b.rocketos.com/members/os_add.php?url='+img_obj.src+'&page_url='+window.location.href,
+			onMessage: function(message, origin){
+				if(message === 'close') {
+					
+				}
+				if(message === 'get_info') {
+					var t_msg = 'INF' + img_obj.src + '|' + window.location.href + '|' + window.location.href;
+					socket.postMessage(t_msg);
+				}
+			},
+			onReady: function() {
+			},
+			container: upload_screen, 
+			props: {style: {width: '100%', height: '100%'}}
+		});
+	};
+	
+	// Draw
+	var obscure = addElement('div', document.body, {
+			position: 'fixed',
+			width: '100%',
+			height: '100%',
+			top: '0px',
+			left: '0px',
+			backgroundColor: 'rgba(0,0,0,0.7)'
+			
+		});
+	var main_container = addElement('div', obscure, {
+		position: 'fixed',
+		width: '80%',
+		height: '500px',
+		left: '50%',
+		top: '50%',
+		margin: '-300px 0 0 -40%',
+		backgroundColor: 'white',
+		border: 'thin solid black',
+		boxShadow: '0px 2px 4px black',
+		overflowY: 'auto',
+		overflowX: 'hidden'
+	});
+	var title_div = addElement('div', main_container, {
+		fontSize: '36px',
+		color: 'black',
+		fontFamily: 'arial',
+		fontWeight: 'bold',
+		boxShadow: '3px 0px 5px rgb(120,120,120)',
+		paddingLeft: '10px',
+		height: '55px',
+		lineHeight: '55px',
+		position: 'relative',
+		zIndex: '1'
+	});
+	title_div.innerHTML = 'Select an image';
+	var images_screen = addElement('div', main_container, {
+		position: 'absolute',
+		top: '55px',
+		bottom: '0px',
+		width: '100%',
+		overflow: 'auto',
+		zIndex: '0'
+	});
+	var upload_screen = addElement('div', main_container, {
+		position: 'absolute',
+		top: '55px',
+		bottom: '0px',
+		width: '100%',
+		overflow: 'auto',
+		display: 'none'
+	});
+
+	function init() {
+		getAndDrawImages();
+	}
+	loadScript('http://www.tylerbiscoe.com/vb/easyXDM/easyXDM.debug.js', function() {
+		init();
+	});
 })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
