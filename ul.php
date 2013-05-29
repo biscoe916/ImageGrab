81a841f7776e8af8b8f92e60e69aff258686f87e
biscoe916 <biscoe@gmail.com>
2013-05-29 16:12:22 -0400

    Update ul.php

--- ul.php
+++ ul.php
@@ -1,25 +1,52 @@
 <?php
-	// Secret key. This must match the secret key in bookmarklet.js
-	$secret_key = "CHANGEME";
-	// Get/Set values
-	$url = trim($_GET["img_url"]);
-	$sk = urldecode($_GET["secret_key"]);
-	$directory = urldecode($_GET["directory"]);
-	$resp_str = $_GET['resp_str'];
-	$new_image_name = rand(1,9999).basename($url);
-	if(!$url || !$directory || !$sk || !$resp_str) { // Ensure we have all of the required data
+// Secret key. This must match the secret key in bookmarklet.js
+$secret_key = "CHANGEME";
+// Get/Set values
+$url = trim($_GET["img_url"]);
+$sk = urldecode($_GET["secret_key"]);
+$directory = urldecode($_GET["directory"]);
+$resp_str = $_GET['resp_str'];
+$new_image_name = rand(1,9999).basename($url);
+
+if(!$url || !$directory || !$sk || !$resp_str) { // Ensure we have all of the required data
+	$return['status'] = 'failed';
+	$return['fail_reason'] = 'Missing Data';
+} elseif($secret_key != $sk) { // Make sure we're authorized
+	$return['status'] = 'failed';
+	$return['fail_reason'] = 'Unauthorized: Keys must match.';
+} else {
+	if(file_put_contents($new_img, file_get_contents($url))) {
+		$return['status'] = 'success';
+		$return['url'] = $directory.$new_img;
+	} else {
 		$return['status'] = 'failed';
-		$return['fail_reason'] = 'Missing Data';
-	} elseif($secret_key != $sk) { // Make sure we're authorized
-		$return['status'] = 'failed';
-		$return['fail_reason'] = 'Unauthorized: Keys must match.';
-	} else { // Now save the file
-		if(file_put_contents($new_img, file_get_contents($url))) {
-			$return['status'] = 'success';
-			$return['url'] = $directory.$new_img;
-		} else {
-			$return['status'] = 'failed';
-			$return['fail_reason'] = 'Unknown Error';
-		}
+		$return['fail_reason'] = 'Unknown Error';
 	}
+}
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
