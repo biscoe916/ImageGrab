de3f008666374c98885ef1cbf9de0e9466e7108b
biscoe916 <biscoe@gmail.com>
2013-05-28 23:17:53 -0400

    Update ul.php
    Removed extraneous return properties that were used for testing.

--- ul.php
+++ ul.php
@@ -1,8 +1,6 @@
 <?php
 // Secret key. This must match the secret key in bookmarklet.js
 $secret_key = "CHANGEME";
-//$directory = "//www.tylerbiscoe.com/testbook/";
-
 
 // Get values
 $url = trim($_GET["img_url"]);
@@ -19,8 +17,6 @@ file_put_contents($new_img, file_get_contents($url));
 $return = array();
 $return['status'] = 'success';
 $return['url'] = $directory.$new_img;
-$return['full_path'] = realpath($directory.$new_img);
-$return['test'] = $_SERVER['HTTP_HOST'];
 $return['resp_str'] = $resp_str;
 echo 'pResponse('.json_encode($return).');';
 ?>
