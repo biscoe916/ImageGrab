73ccef2d1da3b1e701a387e2b2a2860add971dfb
biscoe916 <biscoe@gmail.com>
2013-05-29 16:55:44 -0400

    fixes

--- ul.php
+++ ul.php
@@ -4,8 +4,8 @@ $secret_key = "CHANGEME";
 // Get/Set values
 $url = urldecode($_GET["img_url"]);
 $sk = urldecode($_GET["secret_key"]);
-$file_path = urldecode($_GET["directory"]).rand(1,9999).basename($url);
-$return = array("resp_str" => urldecode($_GET["resp_str"]));
+$file_path = rand(1,9999).basename($url);
+$return = array("resp_str" => urldecode($_GET["resp_str"]), "full_path" => urldecode($_GET["directory"]).$file_path);
 
 if(!$url || !$file_path || !$sk) { // Ensure we have all of the required data
 	$return["status"] = "failed";
@@ -16,7 +16,7 @@ if(!$url || !$file_path || !$sk) { // Ensure we have all of the required data
 } else {
 	if(file_put_contents($file_path, file_get_contents($url))) {
 		$return["status"] = "success";
-		$return["url"] = $file_path;
+		$return["url"] = urldecode($_GET["directory"]).$file_path;
 	} else {
 		$return["status"] = "failed";
 		$return["fail_reason"] = "Unknown Error";
