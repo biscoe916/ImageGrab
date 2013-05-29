5e8118e513aa3ef05d7902664ccc9785768598e9
biscoe916 <biscoe@gmail.com>
2013-05-29 16:09:56 -0400

    Update ul.php

--- ul.php
+++ ul.php
@@ -1,29 +1,26 @@
 <?php
-// Secret key. This must match the secret key in bookmarklet.js
-$secret_key = "CHANGEME";
-
-// Get values
-$url = trim($_GET["img_url"]);
-$sk = urldecode($_GET["secret_key"]);
-$directory = urldecode($_GET["directory"]);
-$resp_str = $_GET['resp_str'];
-
-// Return object
-$return = array();
-$return['resp_str'] = $resp_str;
-
-// Attempt to copy the file
-if(!$url || !$directory || !$sk || !$resp_str) {
-	$return['status'] = 'failed';
-	$return['fail_reason'] = 'Insufficient Data';
-} elseif($secret_key != $sk) {
-	$return['status'] = 'failed';
-	$return['fail_reason'] = 'Unauthorized: Keys must match.';
-} else {
-	$new_img = rand(1,9999).basename($url);
-	file_put_contents($new_img, file_get_contents($url));
-	$return['status'] = 'success';
-	$return['url'] = $directory.$new_img;
-}
-echo 'pResponse('.json_encode($return).');';
+	// Secret key. This must match the secret key in bookmarklet.js
+	$secret_key = "CHANGEME";
+	// Get/Set values
+	$url = trim($_GET["img_url"]);
+	$sk = urldecode($_GET["secret_key"]);
+	$directory = urldecode($_GET["directory"]);
+	$resp_str = $_GET['resp_str'];
+	$new_image_name = rand(1,9999).basename($url);
+	
+	if(!$url || !$directory || !$sk || !$resp_str) { // Ensure we have all of the required data
+		$return['status'] = 'failed';
+		$return['fail_reason'] = 'Missing Data';
+	} elseif($secret_key != $sk) { // Make sure we're authorized
+		$return['status'] = 'failed';
+		$return['fail_reason'] = 'Unauthorized: Keys must match.';
+	} else {
+		if(file_put_contents($new_img, file_get_contents($url))) {
+			$return['status'] = 'success';
+			$return['url'] = $directory.$new_img;
+		} else {
+			$return['status'] = 'failed';
+			$return['fail_reason'] = 'Unknown Error';
+		}
+	}
 ?>
