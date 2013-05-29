894202f2ba4231d0aa3c62719d716820177e6bbc
biscoe916 <biscoe@gmail.com>
2013-05-29 11:15:08 -0300

    Update ul.php

--- ul.php
+++ ul.php
@@ -9,20 +9,21 @@ $directory = urldecode($_GET["directory"]);
 $resp_str = $_GET['resp_str'];
 // Do we have all of the data?
 if(!$url || !$directory || !$sk || !$resp_str) {
-	exit;
-}
-// Save
-if($secret_key == $sk) {
+	$return['status'] = 'failed';
+	$return['fail_reason'] = 'Insufficient Data';
+	$return['resp_str'] = $resp_str;
+	echo 'pResponse('.json_encode($return).');';
+} else if($secret_key != $sk) {
+	$return['status'] = 'failed';
+	$return['fail_reason'] = 'Unauthorized: Keys must match.';
+	$return['resp_str'] = $resp_str;
+	echo 'pResponse('.json_encode($return).');';
+} else {
 	$new_img = rand(1,9999).basename($url);
 	file_put_contents($new_img, file_get_contents($url));
 	$return = array();
 	$return['status'] = 'success';
 	$return['url'] = $directory.$new_img;
-	$return['resp_str'] = $resp_str;
-	echo 'pResponse('.json_encode($return).');';
-} else {
-	$return['status'] = 'failed';
-	$return['fail_reason'] = 'Unauthorized: Keys must match.';
 	$return['resp_str'] = $resp_str;
 	echo 'pResponse('.json_encode($return).');';
 }
